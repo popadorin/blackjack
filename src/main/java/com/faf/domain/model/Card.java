a1a4804d994f302cb62354cb06ce49c733637ca0
Vasile Schidu <sckeedoo@gmail.com>
2017-09-13 18:57:57 +0300

    Create package models

---
+++ src/main/java/com/faf/domain/model/Card.java
@@ -0,0 +1,13 @@
+package com.faf.domain.model;
+
+/**
+ * Created by schiduvasile on 9/13/17.
+ */
+public class Card {
+    private boolean isAce = false;
+    private char value;
+    private char suit;
+    private int cardValue;
+
+
+}
