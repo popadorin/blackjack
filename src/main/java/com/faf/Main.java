b0eff4b5ac990afac65bf53caee480e3f4d0affd
Vasile Schidu <sckeedoo@gmail.com>
2017-09-18 19:17:46 +0300

    Adding class Card

--- src/main/java/com/faf/Main.java
+++ src/main/java/com/faf/Main.java
@@ -1,6 +1,7 @@
 package com.faf;
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello Vasea!");
     }
--- src/main/java/com/faf/domain/model/Card.java
+++ src/main/java/com/faf/domain/model/Card.java
@@ -4,10 +4,46 @@ package com.faf.domain.model;
  * Created by schiduvasile on 9/13/17.
  */
 public class Card {
+
+    public static final char[] suits = {'C', 'D', 'S', 'H'};
+    public static final char[] values = {'A', '2', '3', '4', '5', '6', '7', '8', '9', 'X', 'J', 'Q', 'K'};
+
     private boolean isAce = false;
     private char value;
     private char suit;
-    private int cardValue;
+    private int points;
+
+    public Card(char value, char suit) {
+        this.suit = suit;
+        this.value = value;
+
+        switch (value) {
+            case 'A':
+                points = 11;
+                break;
+            case 'X':
+            case 'J':
+            case 'Q':
+            case 'K':
+                points = 10;
+                break;
+            default:
+                points = Character.getNumericValue(value);
+        }
+    }
+
+    public int getPoints(int handValue) {
+        if(isAce) {
+            return (handValue < 11) ? 11 : 1;
+        } else {
+            return points;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return " " + value + suit;
+    }
 
 
 }
